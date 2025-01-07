3c50bca78f412c2b4780de14ef1992e8d04f6047
aeerema <eremchenko@informatics.ru>
2025-01-07 05:16:21 +0300

    Добавил возможность удалять изображения

--- fut_in_pst_typology/static/js/imageShow.js
+++ fut_in_pst_typology/static/js/imageShow.js
@@ -6,3 +6,16 @@ function turnOnImageShow(img) {
 function turnOffImageShow(modalContainer) {
     modalContainer.classList.toggle('active');
 }
+
+function deleteImage(button) {
+    data = new FormData(button.nextElementSibling);
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', document.URL, true);
+
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            document.getElementById("image_"+data.get("delete_image")).remove();
+        }
+    }
+    xhr.send(data);
+}
